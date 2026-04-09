5eefb0a1fc21bdd56a82671006b285ae4ca4c0a5
ADMIN <vumaiquan28062002@gmail.com>
2026-04-09 23:22:02 +0700

    Leen code moiw

--- src/main/java/com/example/realtimechat/db1/repositories/NguoiDungRepository.java
+++ src/main/java/com/example/realtimechat/db1/repositories/NguoiDungRepository.java
@@ -3,7 +3,6 @@ package com.example.realtimechat.db1.repositories;
 import com.example.realtimechat.db1.model.NguoiDung;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 import java.util.UUID;
