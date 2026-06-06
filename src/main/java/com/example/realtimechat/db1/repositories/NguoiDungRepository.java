e699dad0afbec913e01a652fa8607cfd25fa0ba7
Admin <vumaiquan28062002@gmail.com>
2026-06-06 10:47:47 +0700

    Chỉnh sửa validation unique

--- src/main/java/com/example/realtimechat/db1/repositories/NguoiDungRepository.java
+++ src/main/java/com/example/realtimechat/db1/repositories/NguoiDungRepository.java
@@ -1,21 +1,16 @@
 package com.example.realtimechat.db1.repositories;
 
 import com.example.realtimechat.db1.model.NguoiDung;
-import com.example.realtimechat.db1.model.NguoiDung_;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface NguoiDungRepository extends FieldExist, JpaRepository<NguoiDung, UUID>, JpaSpecificationExecutor<NguoiDung> {
+public interface NguoiDungRepository extends FieldExist, JpaRepository<NguoiDung, UUID>{
     boolean existsByEmail(@NonNull String email);
 
     Optional<NguoiDung> findByEmail(@NonNull String email);
@@ -25,14 +20,6 @@ public interface NguoiDungRepository extends FieldExist, JpaRepository<NguoiDung
     @Transactional
     void updateVersionToken(UUID id, UUID newValue);
 
-    default Specification<NguoiDung> findLessThan() {
-
-        return (root, query, cb) -> {
-            assert query != null;
-            query.groupBy(root.get(NguoiDung_.email)).having(cb.lessThan(root.get(NguoiDung_.creationTime), LocalDateTime.now()));
-            return cb.and(cb.lessThan(root.get(NguoiDung_.email), ""), cb.lessThan(root.get(NguoiDung_.email), ""));
-        };
-    }
     @Override
     default boolean fieldExistByValue(String value) {
         return existsByEmail(value);
